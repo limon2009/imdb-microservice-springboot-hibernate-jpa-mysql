353e4d07deed88e8b7457b9dee9ff000929a2f71
Md Moniruzzaman <limon2009@gmail.com>
2022-09-22 11:33:51 +0100

    Movie Service Initial commit

---
+++ imdb-movie-microservice/src/test/java/com/imdb/movie/ImdbMovieMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdb.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbMovieMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
